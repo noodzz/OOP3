254c5a370c6eceba2b3f9b91ff9bf3bfc61ef21e
noodzz <102947953+noodzz@users.noreply.github.com>
2022-09-27 14:10:02 +0500

    добавлены методы класса MyStorage

--- OOP3/OOP3.cpp
+++ OOP3/OOP3.cpp
@@ -55,6 +55,10 @@ public:
         delete[] arr;
         arr = tmp;
     }
+    void push_front(const T& element)
+    {
+        insert(element, 0);
+    }
     void clear()
     {
         delete[] arr;
@@ -73,6 +77,20 @@ public:
         }
         else return;
     }
+    void erase(int pos)
+    {
+        if (len != 0 && arr != nullptr) {
+            len -= 1;
+            T* tmp = new T[len];
+            for (int i = 0; i < pos; i++)
+                tmp[i] = arr[i];
+            for (int i = pos; i < len; i++)
+                tmp[i] = arr[i+1];
+            delete[] arr;
+            arr = tmp;
+        }
+        else return;
+    }
     int size()
     {
         return len;
@@ -81,14 +99,57 @@ public:
     {
         return arr[index];
     }
+    void get_and_erase(int index)
+    {
+        cout << arr[index] << endl;;
+        erase(index);
+    }
+    bool empty()
+    {
+        if (len == 0 || arr == nullptr) return true;
+        else return true;
+    }
+    T begin()
+    {
+        return arr[0];
+    }
+    T end()
+    {
+        return arr[len - 1];
+    }
+    T* next(T* it)
+    {
+        T* ptr = it + 1;
+        return ptr;
+    }
+    T* next(T* it, int n)
+    {
+        T* ptr = it + n;
+        return ptr;
+    }
+    T* prev(T* it)
+    {
+        if (it != 0)
+        {
+            T* ptr = it - 1;
+            return ptr;
+        }
+    }
+    T* prev(T* it, int n)
+    {
+        if (it - n >= 0) {
+            T* ptr = it - n;
+            return ptr;
+        }
+    }
 };
 
 int main()
 {
     MyStorage <int> m(10);
     m.push_back(1);
-    cout << m.get(9) << endl;
-    for (int i = 0; i < m.size(); i++)
-        cout << m[i] << " ";
+    m.push_front(2);
+    m[5] = 5;
+    cout << m.end();
 
 }
